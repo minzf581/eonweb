c91517d428f630a24278af22f048fc6f3795d324
W3Router <info@w3airouter.com>
2024-12-09 08:36:03 +0800

    Update CORS configuration with additional headers

--- server.js
+++ server.js
@@ -13,5 +13,17 @@ app.use(cors({
     origin: 'https://w3router.github.io',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-})); 
+    allowedHeaders: ['Content-Type', 'Authorization', 'Origin'],
+    exposedHeaders: ['Access-Control-Allow-Origin'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+}));
+
+// 添加额外的 CORS 头
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://w3router.github.io');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+});
