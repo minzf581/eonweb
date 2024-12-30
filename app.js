100136d18c088544695c3bee354cd44a4547603f
W3Router <minzf581@gmail.com>
2024-12-30 10:17:49 +0800

    2

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const tasksRoutes = require('./routes/tasks');
 const statsRoutes = require('./routes/stats');
 const adminRoutes = require('./routes/admin');
 const pointsRoutes = require('./routes/points');
+const http = require('http');
 
 const app = express();
 
@@ -42,6 +43,34 @@ app.use((err, req, res, next) => {
         message: 'Something broke!',
         error: process.env.NODE_ENV === 'development' ? err.message : undefined
     });
+});
+
+// Create a bare minimum server just for health checks
+const server = http.createServer((req, res) => {
+    if (req.url === '/_ah/start') {
+        console.log('Received App Engine start request');
+        res.writeHead(200);
+        res.end('OK');
+        
+        // Start the main application in the background
+        require('./app');
+    } 
+    else if (req.url === '/_ah/stop') {
+        console.log('Received App Engine stop request');
+        res.writeHead(200);
+        res.end('OK');
+    }
+    else {
+        // Forward all other requests to the main application
+        require('./app');
+        res.writeHead(404);
+        res.end();
+    }
+});
+
+const port = process.env.PORT || 8081;
+server.listen(port, () => {
+    console.log(`Health check server listening on port ${port}`);
 });
 
 module.exports = app;
