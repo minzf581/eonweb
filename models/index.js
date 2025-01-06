7a76deb0c95ba4c12ced24031ff51fefa2cbe9ca
W3Router <minzf581@gmail.com>
2025-01-06 15:55:38 +0800

    1

--- models/index.js
+++ models/index.js
@@ -9,6 +9,7 @@ const initPointHistory = require('./PointHistory');
 const initSettings = require('./Settings');
 const initReferral = require('./Referral');
 const initProxyApiKey = require('./ProxyApiKey');
+const initNodeStatus = require('./NodeStatus');
 
 // Initialize models
 const User = initUser(sequelize);
@@ -18,6 +19,7 @@ const PointHistory = initPointHistory(sequelize);
 const Settings = initSettings(sequelize);
 const Referral = initReferral(sequelize);
 const ProxyApiKey = initProxyApiKey(sequelize);
+const NodeStatus = initNodeStatus(sequelize);
 
 // Define models object
 const models = {
@@ -28,6 +30,7 @@ const models = {
     Settings,
     Referral,
     ProxyApiKey,
+    NodeStatus,
     sequelize
 };
 
@@ -35,15 +38,18 @@ const models = {
 User.hasMany(PointHistory);
 PointHistory.belongsTo(User);
 
-User.hasMany(UserTask, { as: 'UserTasks' });
-Task.hasMany(UserTask, { as: 'UserTasks' });
-UserTask.belongsTo(User, { as: 'User' });
-UserTask.belongsTo(Task, { as: 'Task' });
+User.hasMany(UserTask, { as: 'userTasks' });
+Task.hasMany(UserTask, { as: 'userTasks' });
+UserTask.belongsTo(User, { as: 'user' });
+UserTask.belongsTo(Task, { as: 'task' });
 
-User.hasMany(Referral, { foreignKey: 'referrerId', as: 'Referrals' });
-User.hasMany(Referral, { foreignKey: 'referredId', as: 'ReferredBy' });
-Referral.belongsTo(User, { foreignKey: 'referrerId', as: 'Referrer' });
-Referral.belongsTo(User, { foreignKey: 'referredId', as: 'Referred' });
+User.hasMany(Referral, { foreignKey: 'referrerId', as: 'referrals' });
+User.hasMany(Referral, { foreignKey: 'referredId', as: 'referredBy' });
+Referral.belongsTo(User, { foreignKey: 'referrerId', as: 'referrer' });
+Referral.belongsTo(User, { foreignKey: 'referredId', as: 'referred' });
+
+User.hasMany(NodeStatus, { foreignKey: 'userId', as: 'nodes' });
+NodeStatus.belongsTo(User, { foreignKey: 'userId', as: 'user' });
 
 // Export models and sequelize instance
 module.exports = models;
