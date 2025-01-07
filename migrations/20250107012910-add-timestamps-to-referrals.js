3da46d3bc35b036b4bfc493ef5861d3414fdce2b
W3Router <minzf581@gmail.com>
2025-01-07 09:37:30 +0800

    2

--- migrations/20250107012910-add-timestamps-to-referrals.js
+++ migrations/20250107012910-add-timestamps-to-referrals.js
@@ -3,25 +3,14 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
+    // Only add deleted_at since created_at and updated_at already exist
     await queryInterface.addColumn('referrals', 'deleted_at', {
       type: Sequelize.DATE,
       allowNull: true
-    });
-    await queryInterface.addColumn('referrals', 'created_at', {
-      type: Sequelize.DATE,
-      allowNull: false,
-      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-    });
-    await queryInterface.addColumn('referrals', 'updated_at', {
-      type: Sequelize.DATE,
-      allowNull: false,
-      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
     });
   },
 
   async down(queryInterface, Sequelize) {
     await queryInterface.removeColumn('referrals', 'deleted_at');
-    await queryInterface.removeColumn('referrals', 'created_at');
-    await queryInterface.removeColumn('referrals', 'updated_at');
   }
 };
