0b310dd144183abd03b2b508a393a91e53ff1fb2
minzf581 <minzf581@gmail.com>
2025-01-15 18:39:08 +0800

    1

--- server.js
+++ server.js
@@ -287,14 +287,3 @@ initializeApp().catch(error => {
   console.error('[DEBUG] 应用启动失败:', error);
   process.exit(1);
 });
-
-// 打印导入的代理模块信息
-console.log('[DEBUG] 导入的代理模块:', {
-  type: typeof proxyModule,
-  router: typeof proxyModule.router,
-  handlers: typeof proxyModule.handlers,
-  routes: proxyModule.router?.stack?.map(r => ({
-    path: r.route?.path,
-    methods: r.route?.methods
-  }))
-});
